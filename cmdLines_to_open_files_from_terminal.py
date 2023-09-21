c4fdcef0ddfeb0b78e35bc5a98e560be83677cbf
Subodh <subodh.r.pataki@gmail.com>
2023-09-21 23:33:10 +0530

    command lines to create, rename, and delete files from terminal

---
+++ cmdLines_to_open_files_from_terminal.py
@@ -0,0 +1,17 @@
+# cmd line to create a pyhton file from terminal
+echo. > fileName.py
+
+# cmd line to open the python file from terminal
+code fileName.py
+
+# create and open python file from terminal 
+echo. > fileName.py && code fileName.py
+
+# short-cut ( open directly then -> save it )
+code fileName.py -> then (ctrl + S)
+
+# rename the file
+rename old_fileName new_fileName
+
+# delete the file (for windows)
+del path\to\file 
